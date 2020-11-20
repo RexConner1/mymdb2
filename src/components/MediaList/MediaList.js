c97aaed6aa2aad4e509d72437d151d91d46ee5ed
David Orona <OronaDavidJ@JohnDeere.com>
2020-11-20 10:04:52 -0600

    Send details to Details page

--- src/components/MediaList/MediaList.js
+++ src/components/MediaList/MediaList.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 class MediaList extends Component {
     render() {
+        const media = this.props.media.map(singleMedia => {
+            <DetailPage media={singleMedia} key={singleMedia.id} />
+        })
+
         return (
             <div>
                 <p>MediaList</p>
