113597ce6001650d8fc37a6d67fc8bac0262687e
David Orona <OronaDavidJ@JohnDeere.com>
2020-11-20 20:00:08 -0600

    Functions for APIs

--- src/components/MediaList/MediaList.js
+++ src/components/MediaList/MediaList.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 class MediaList extends Component {
+    searchForTitle = async() => {
+        const response = await axios.get('https://api.themoviedb.org/3/search/tv?api_key=66b22cb2598318b06f69e25cb751c2ad&query=Lost')
+        console.log(response.data.results);
+    }
+
+    searchForUPC = async(upcNumber='786936858136') => {
+        const response = await axios.get(`https://api.upcdatabase.org/product/786936858136?apikey=2F1653F46E6223E30135DB3D80B8C22D`)
+        console.log(response.data);
+    }
+
     render() {
         const media = this.props.movies ? this.props.movies : this.props.shows
         const linksToMedia = media.map(item => {
@@ -16,8 +27,8 @@ class MediaList extends Component {
         return (
             <div>
                 <form onSubmit={(e) => {
-                    e.preventDefault();
-                    console.log('Hi')
+                    e.preventDefault()
+                    this.searchForTitle()
                 }}>
                     <label htmlFor="titleSearch">Title Search:</label>
                     <input name="titleSearch" id="titleSearch" />
@@ -25,7 +36,7 @@ class MediaList extends Component {
                 </form>
                 <form onSubmit={(e) => {
                     e.preventDefault();
-                    console.log('Hello')
+                    this.searchForUPC()
                 }}>
                     <label htmlFor="upcSearch">UPC Search:</label>
                     <input name="upcSearch" id="upcSearch" />
