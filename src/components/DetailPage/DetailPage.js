d6d0653735bdd3cc2560d5b500b6976a6d5a1e5f
Unknown <unknown@example.com>
2020-11-22 21:51:09 -0500

    overview and date rendering

--- src/components/DetailPage/DetailPage.js
+++ src/components/DetailPage/DetailPage.js
@@ -2,28 +2,7 @@ import React, { Component } from "react";
 
 class DetailPage extends Component {
     render(){
-// let DetailPage = props => {
-//     let details 
 
-//     let posterUrl = ''
-//     let backdropUrl = ''
-
-    // if (movie.id){
-    //     details =(
-
-    //     )
-
-    // } 
-    // if (tvShow.id){
-    //     details =(
-
-    //     )
-
-    // } else {
-    //     details =(
-
-    //     )
-    // }
     let media = this.props.movies ? this.props.movies : this.props.shows
     media = media.find(info => info.id == this.props.match.params.id)
 
@@ -31,6 +10,9 @@ class DetailPage extends Component {
         <div className='film-details'>
             <h1>DETAILS</h1>
             <h2>{this.props.movies ? media.title : media.name}</h2>
+            <p>{this.props.movies ? media.releaseDate : media.first_air_date}</p>
+            <h3>{this.props.movies ? media.overview : media.overview}</h3>
+            
             {/* {details} */}
         </div>
     )
