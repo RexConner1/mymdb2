7540b420a575a8d8917a93c2570b3bf625aabf84
Unknown <unknown@example.com>
2020-11-22 23:00:14 -0500

    Poster Rendering

--- src/components/DetailPage/DetailPage.js
+++ src/components/DetailPage/DetailPage.js
@@ -6,12 +6,23 @@ class DetailPage extends Component {
     let media = this.props.movies ? this.props.movies : this.props.shows
     media = media.find(info => info.id == this.props.match.params.id)
 
+    let poster = this.props.movies ? media.poster_path : media.poster_path;
+    let posterURL = "https://image.tmdb.org/t/p/w780/" + poster; 
+    
+
+    console.log(this.props)
+    console.log(posterURL)
+
     return (
+
         <div className='film-details'>
             <h1>DETAILS</h1>
             <h2>{this.props.movies ? media.title : media.name}</h2>
-            <p>{this.props.movies ? media.releaseDate : media.first_air_date}</p>
-            <h3>{this.props.movies ? media.overview : media.overview}</h3>
+            <p>{this.props.movies ? media.release_date : media.first_air_date}</p>
+            <h3>{this.props.movies ? media.overview : media.overview}</h3> 
+            {/* <p>{this.props.movies ? media.poster_path : media.poster_path}</p> */}
+
+            <img src={posterURL} alt="Poster Image"/>
             
             {/* {details} */}
         </div>
